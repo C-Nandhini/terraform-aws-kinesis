9566eebadbb5dc66ccb8be1171682de1188b7364
C-Nandhini <40636098+C-Nandhini@users.noreply.github.com>
2021-04-30 15:13:50 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,6 +1,6 @@
 # create vpc
 
-resource "aws_vpc" "main" {
+resource "aws_vpcc" "main" {
   cidr_block                     = var.cidr_block
   instance_tenancy               = var.instance_tenancy
   enable_dns_support             = var.enable_dns_support
