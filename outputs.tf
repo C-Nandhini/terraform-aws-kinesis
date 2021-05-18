dd4b2b6b7c0dc33ac81c336b867508a2ef035c14
C-Nandhini <nishaa18nandy@gmail.com>
2021-05-18 19:18:35 +0530

    testing go

--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,22 @@
 # sample outputs
 
 output "vpc_arn" {
-  value = module.vpc[*].arn
+  value = module.vpc.arn
   description = "vpc-arn"
 }
 
 output "vpc_id" {
-  value = module.vpc[*].id
+  value = module.vpc.id
   description = "vpc-id"
 }
 
 output "internet_gateway_arn" {
-  value = module.igw[*].arn
+  value = module.igw.arn
   description = "igw-arn"
 }
 
 output "internet_gateway_id" {
-  value = module.igw[*].id
+  value = module.igw.id
   description = "igw-id"
 }
 
