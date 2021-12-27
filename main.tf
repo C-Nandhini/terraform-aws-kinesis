d6452a6e25743cdaf9abd19d65eecb4ba4a63f25
C-Nandhini <40636098+C-Nandhini@users.noreply.github.com>
2021-12-27 12:57:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,20 @@
     vpc_id      = module.vpc.id
  }
 
+ resource "aws_ssm_parameter" "sample" {
+  name            = "sample"
+  type            = "String"
+  value           = "test"
+  description     = "policy test"
+  tier            = "Standard"
+  allowed_pattern = ".*"
+  data_type       = "text"
+  tags            = {
+    created_for = "BC Policy Test"
+  }
+}
+
+
 
 
 
