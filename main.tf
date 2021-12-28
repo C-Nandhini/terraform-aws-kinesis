0afb26c0e2d19a6b188d4dcc8299a038a8175718
C-Nandhini <40636098+C-Nandhini@users.noreply.github.com>
2021-12-28 10:50:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,98 @@
- module "vpc" {
-   source      = "./modules/rename-me-to-primary-resource-example-vpc"
+#  module "vpc" {
+#    source      = "./modules/rename-me-to-primary-resource-example-vpc"
 
-   cidr_block  = var.cidr_block
- }
+#    cidr_block  = var.cidr_block
+#  }
 
- module "igw" {
-    source      = "./modules/rename-me-to-secondary-resource-example-igw"
+#  module "igw" {
+#     source      = "./modules/rename-me-to-secondary-resource-example-igw"
 
-    vpc_id      = module.vpc.id
- }
+#     vpc_id      = module.vpc.id
+#  }
 
- resource "aws_ssm_parameter" "sample" {
-  name            = "sample"
-  type            = "String"
-  value           = "test"
-  description     = "policy test"
-  tier            = "Standard"
-  allowed_pattern = ".*"
-  data_type       = "text"
-  tags            = {
-    created_for = "BC Policy Test"
+// Configure Terraform Provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
   }
+}
+
+// Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"
+}
+
+locals {
+  policy = <<EOL
+{
+   "Version": "2012-10-17",
+   "Id": "Policy1415115909152",
+   "Statement": [
+     {
+       "Sid": "Access-to-specific-VPCE-only",
+       "Principal": "*",
+       "Action": "s3:*",
+       "Effect": "Deny",
+       "Resource": "*"
+       "Condition": {
+         "StringNotEquals": {
+           "aws:SourceVpce": "vpce-1a2b3c4d"
+         }
+       }
+     },
+     {
+       "Sid": "Access-to-specific-VPCE-only",
+       "Principal": "*",
+       "Action": "s3:*",
+       "Effect": "Allow",
+       "Resource": "*"
+       "Condition": {
+         "StringEquals": {
+           "aws:SourceVpce": "vpce-1a2b3c4d"
+         }
+       }
+     },
+   ]
+}
+EOL
+}
+
+resource "aws_s3_bucket" "sample" {
+  bucket = "sample-${random_integer.random.id}"
+  acl    = "private"
+  policy = local.policy
+}
+
+
+resource "aws_s3_bucket" "sample1" {
+  bucket = "sample1-${random_integer.random.id}"
+}
+
+resource "aws_s3_bucket_policy" "sample1_policy" {
+  bucket = aws_s3_bucket.sample1.id
+  policy = local.policy
+}
+
+resource "random_integer" "random" {
+  min = 1
+  max = 99999999
+}
+
+
+resource "aws_ssm_parameter" "sample" {
+ name            = "sample"
+ type            = "String"
+ value           = "test"
+ description     = "policy test"
+ tier            = "Standard"
+ allowed_pattern = ".*"
+ data_type       = "text"
+ tags            = {
+   created_for = "BC Policy Test"
+ }
 }
 
 
