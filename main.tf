a9c5aced943f6b4b21d8bdd5d66029d78e4acdc1
C-Nandhini <40636098+C-Nandhini@users.noreply.github.com>
2021-12-28 18:21:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -126,7 +126,18 @@ resource "aws_ssm_parameter" "sample" {
  }
 }
 
-
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  backup_retention_period = 1
+}
 
 
 
