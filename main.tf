3b4f72d53cf4c40031a0d821878a68c243712be0
C-Nandhini <40636098+C-Nandhini@users.noreply.github.com>
2022-01-09 17:26:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -210,6 +210,16 @@ resource "aws_route_table" "aws_route_table_not_ok" {
   }
 }
 
+
+resource "aws_route_table" "aws_route_table_test" {
+  vpc_id = aws_vpc.example.id
+
+  route {
+    cidr_block = "10.0.0.0/24"
+    instance_id = aws_instance.example.id
+  }
+}
+
 resource "aws_route" "aws_route_ok" {
   route_table_id            = aws_route_table.example.id
   destination_cidr_block    = "0.0.0.0/0"
@@ -219,5 +229,11 @@ resource "aws_route" "aws_route_ok" {
 resource "aws_route" "aws_route_not_ok" {
   route_table_id            = aws_route_table.example.id
   destination_cidr_block    = "0.0.0.0/0"
+  instance_id               = aws_instance.example.id
+}
+
+resource "aws_route" "aws_route_test" {
+  route_table_id            = aws_route_table.example.id
+  destination_cidr_block    = "10.0.0.0/24"
   instance_id               = aws_instance.example.id
 }
