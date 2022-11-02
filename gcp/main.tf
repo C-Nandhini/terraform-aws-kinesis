7b639148a8c116fc5295d026b574a626b5c70be2
C-Nandhini <40636098+C-Nandhini@users.noreply.github.com>
2022-11-02 20:15:54 +0530

    Update main.tf

--- gcp/main.tf
+++ gcp/main.tf
@@ -94,3 +94,20 @@ resource "google_compute_firewall" "enabled-ingress" {
   direction = "INGRESS"
   
 }
+
+# firewall -> enabled, INGRESS, not allowed all -> false
+
+resource "google_compute_firewall" "enabled-ingress-not-allowed" {
+  name    = "test-firewall"
+  network = google_compute_network.default.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "1000-2000"]
+  }
+
+  source_tags = ["web"]
+  
+  direction = "INGRESS"
+  
+}
