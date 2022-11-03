9c98051cc0dea87794f4beed7a30a09f101f910d
C-Nandhini <40636098+C-Nandhini@users.noreply.github.com>
2022-11-03 10:11:34 +0530

    Update main.tf

--- gcp/main.tf
+++ gcp/main.tf
@@ -1,179 +1,89 @@
-provider "google" {
-  project = "pcs-policy-auto"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+resource "google_compute_network" "example" {
+  name = "example"
+  auto_create_subnetworks = false
 }
 
-resource "google_compute_network" "default" {
-  name = "test-network"
-}
+#case1 - PASS
+resource "google_compute_firewall" "compute_firewall_ok_1" {
+  name    = "compute_firewall_ok_1"
+  network = google_compute_network.example.name
 
-# firewall -> disabled, EGRESS -> false
-
-resource "google_compute_firewall" "disabled-egress" {
-  name    = "test-firewall"
-  network = google_compute_network.default.name
-
-  allow {
+  deny {
     protocol = "all"
   }
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "8080", "1000-2000"]
-  }
-
-  source_tags = ["web"]
-  
-  direction = "EGRESS"
-  
-  disabled = true
-}
-
-# firewall -> enabled, EGRESS -> false
-
-resource "google_compute_firewall" "enabled-egress" {
-  name    = "test-firewall"
-  network = google_compute_network.default.name
-
-  allow {
-    protocol = "all"
-  }
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "8080", "1000-2000"]
-  }
-
-  source_tags = ["web"]
-  
-  direction = "EGRESS"
-  
-  disabled = false
-}
-
-# firewall -> disabled, INGRESS -> false
-
-resource "google_compute_firewall" "disabled-ingress" {
-  name    = "test-firewall"
-  network = google_compute_network.default.name
-
-  allow {
-    protocol = "all"
-  }
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "8080", "1000-2000"]
-  }
-
-  source_tags = ["web"]
-  
-  direction = "INGRESS"
-  
-  disabled = true
-}
-
-# firewall -> enabled, INGRESS -> true
-
-resource "google_compute_firewall" "enabled-ingress" {
-  name    = "test-firewall"
-  network = google_compute_network.default.name
-
-  allow {
-    protocol = "all"
-  }
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "8080", "1000-2000"]
-  }
-
-  source_tags = ["web"]
-  
-  direction = "INGRESS"
-  
-}
-
-# firewall -> enabled, INGRESS, not allowed all -> false
-
-resource "google_compute_firewall" "enabled-ingress-not-allowed" {
-  name    = "test-firewall"
-  network = google_compute_network.default.name
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "8080", "1000-2000"]
-  }
-
-  source_ranges = ["0.0.0.0/0","192.168.4.4"]
-  
-  direction = "INGRESS"
-  
-}
-
-# firewall -> enabled, INGRESS, not allowed all -> false
-
-resource "google_compute_firewall" "enabled-ingress-allowed" {
-  name    = "test-firewall"
-  network = google_compute_network.default.name
-
-  allow {
-    protocol = "all"
-  }
-
-  source_ranges = ["0.0.0.0/0","::/0","0.0.0.0","::0"]
-  
-  direction = "INGRESS"
-  
-}
-
-# firewall -> enabled, INGRESS, not allowed all -> false
-
-resource "google_compute_firewall" "enabled-ingress-allowed-1" {
-  name    = "test-firewall"
-  network = google_compute_network.default.name
-
-  allow {
-    protocol = "all"
-  }
-
   source_ranges = ["0.0.0.0/0"]
-  
-  direction = "INGRESS"
-  
-}
-
-# firewall -> enabled, INGRESS, not allowed all -> false
-
-resource "google_compute_firewall" "enabled-ingress-allowed-2" {
-  name    = "test-firewall"
-  network = google_compute_network.default.name
-  
-  allow {
-    protocol = "all"
-  }
-
-  source_ranges = "0.0.0.0/0"
-  
-  direction = "INGRESS"
-  
   disabled = false
-  
 }
 
-# firewall -> enabled, INGRESS, allowed all -> false
-
-resource "google_compute_firewall" "enabled-ingress-allowed3" {
-  name    = "test-firewall"
-  network = google_compute_network.default.name
+#case2 - PASS
+resource "google_compute_firewall" "compute_firewall_ok_2" {
+  name    = "compute_firewall_ok_2"
+  network = google_compute_network.example.name
 
   allow {
     protocol = "all"
   }
+  source_ranges = ["::/0"]
+  disabled = true
+}
 
-  source_ranges = ["0.0.0.0/0","192.168.4.4"]
+#case3 - PASS
+resource "google_compute_firewall" "compute_firewall_ok_3" {
+  name    = "compute_firewall_ok_3"
+  network = google_compute_network.example.name
+
+  allow {
+    protocol = "tcp"
+    ports = ["140"]
+  }
+  source_ranges = ["0.0.0.0", "192.168.2.0"]
+  disabled = false
+}
+
+#case4 - FAIL
+resource "google_compute_firewall" "compute_firewall_not_ok_1" {
+  name    = "compute_firewall_not_ok_1"
+  network = google_compute_network.example.name
   
-  direction = "INGRESS"
-  
+  allow {
+    protocol = "all"
+  }
+  source_ranges = ["::/0"]
+  disabled = false
+}
+
+#case5 - FAIL
+resource "google_compute_firewall" "compute_firewall_not_ok_2" {
+  name    = "compute_firewall_not_ok_2"
+  network = google_compute_network.example.name
+
+  allow {
+    protocol = "all"
+  }
+  source_ranges = ["0.0.0.0", "192.168.2.0"]
+  disabled = false
+}
+
+#case6 - FAIL
+resource "google_compute_firewall" "compute_firewall_not_ok_4" {
+  name    = "compute_firewall_not_ok_3"
+  network = google_compute_network.example.name
+
+  allow {
+    protocol = "all"
+  }
+  source_ranges = ["0.0.0.0/0"]
+  disabled = false
+}
+
+#case7 - FAIL
+resource "google_compute_firewall" "compute_firewall_not_ok_4" {
+  name    = "compute_firewall_not_ok_4"
+  network = google_compute_network.example.name
+
+  allow {
+    protocol = "all"
+  }
+  source_ranges = ["::0"]
+  disabled = false
 }
