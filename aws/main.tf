852f31e2f107b39858414a5c1153fbde7a75d458
C-Nandhini <40636098+C-Nandhini@users.noreply.github.com>
2022-11-17 18:54:15 +0530

    Create main.tf

---
+++ aws/main.tf
@@ -0,0 +1,72 @@
+resource "aws_s3_bucket" "test_s3_bucket" {
+  bucket = "tf-cloudfront-s3-bucket"
+}
+
+
+resource "aws_cloudfront_distribution" "test_cloudfront_distribution" {
+  origin = {
+
+  tlsv1 = {
+    domain_name          = "tlsv1.example.com"
+    custom_origin_config = {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "match-viewer"
+      origin_ssl_protocols   = ["TLSv1"]
+    }
+  }
+
+  tlsv2 = {
+    domain_name = "something.example.com"
+    custom_origin_config = {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "match-viewer"
+      origin_ssl_protocols   = ["TLSv2"]
+    }
+  }
+
+  sslv3 = {
+    domain_name = "something.example.com"
+    custom_origin_config = {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "match-viewer"
+      origin_ssl_protocols   = ["SSLv3"]
+    }
+  }
+
+}
+
+  enabled = true
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.test_s3_bucket.id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
