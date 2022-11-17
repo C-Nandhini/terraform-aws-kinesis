fb66743ae97fc10e7313ed0d8c03b2af7e0d3e71
C-Nandhini <40636098+C-Nandhini@users.noreply.github.com>
2022-11-17 19:25:03 +0530

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -70,7 +70,6 @@ resource "aws_cloudfront_distribution" "test_cloudfront_distribution" {
   }
 }
 
-
 resource "aws_cloudfront_distribution" "test_cloudfront_distribution1" {
   origin = {
 
@@ -81,6 +80,54 @@ resource "aws_cloudfront_distribution" "test_cloudfront_distribution1" {
         https_port             = 443
         origin_protocol_policy = "match-viewer"
         origin_ssl_protocols   = ["SSLv3"]
+      }
+    }
+
+  }
+
+  enabled = true
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.test_s3_bucket.id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+resource "aws_cloudfront_distribution" "test_cloudfront_distribution2" {
+  origin = {
+
+    tlsv2 = {
+      domain_name = "something.example.com"
+      custom_origin_config = {
+        http_port              = 80
+        https_port             = 443
+        origin_protocol_policy = "match-viewer"
+        origin_ssl_protocols   = ["TLSv2"]
       }
     }
 
