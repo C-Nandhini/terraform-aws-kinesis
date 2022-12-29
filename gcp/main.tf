e8b79804b01019db06e91f3ab8c5b118f281b069
C-Nandhini <40636098+C-Nandhini@users.noreply.github.com>
2022-12-29 20:47:58 +0530

    Update main.tf

--- gcp/main.tf
+++ gcp/main.tf
@@ -157,3 +157,21 @@ resource "google_sql_database_instance" "postgresql-instance-ok-2" {
   }
   deletion_protection = false
 }
+
+#mysql
+resource "google_sql_database_instance" "mysql" {
+  name             = "nvul-sql-j1-1-rlp-71271"
+  database_version = "MYSQL_8_0"
+  settings {
+    database_flags {
+      name  = "skip_show_database"
+      value = "on"
+    }
+    database_flags {
+      name  = "local_infile"
+      value = "off"
+    }
+    tier = "db-f1-micro"
+  }
+  deletion_protection = false
+}
